d618b82928e30a710bb5de103d6c967a6e7a75f7
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-08-24 12:36:07 -0500

    commit tree b3b4f0501637609442f3f38309641f9a052cb413

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, adj) {
+    let surprise = [];
+    for(let i = 0; i < names.length; i++) {
+        surprise.push(`Thank you, ${names[i]}, for the wonderful ${adj} gift!`);
+    }
+    return surprise;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(int) {
+    for (let int = 10; int >= 0; int--) {
+        console.log(int);
+    }
+}
